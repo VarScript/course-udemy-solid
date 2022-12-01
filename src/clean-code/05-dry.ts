ad8dcb420d67a1828bf854bad34a9b6efaf7f89e
DevHars <vargassanchezfabio01@gmail.com>
2022-12-01 18:41:33 -0500

    Applying principle DRY

--- src/clean-code/05-dry.ts
+++ src/clean-code/05-dry.ts
@@ -8,21 +8,39 @@ class Product {
         public size:Size     = '',
     ) {}
 
+    isProductReady(): boolean {
+
+        for ( const key in this ) {
+            switch( typeof this[key] ) {
+                case 'string':
+                    if ( (<string><unknown>this[key]).length <= 0 ) throw Error(`${key} is empty`);
+                    break;
+                case 'number':
+                    if ( (<number><unknown>this[key]) <= 0 ) throw Error(`${key} is zero`);
+                    break;
+                default: 
+                    throw Error(`${typeof this[key]} is not valid`)
+            }
+        }
+
+        return true;
+    }
+
     toString() {
         // No Dry
-        if( this.name.length <= 0) throw Error('name is empty');
-        if( this.price <= 0)       throw Error('Price is zero');
-        if( this.size.length <= 0) throw Error('size is empty');
+        // if( this.name.length <= 0) throw Error('name is empty');
+        // if( this.price <= 0)       throw Error('Price is zero');
+        // if( this.size.length <= 0) throw Error('size is empty');
 
+        if ( !this.isProductReady ) return;
 
         return `${this.name} (${this.price}), ${this.size}`
     }
 }   
 
 (() => {
-    
-    const bluePants = new Product('Blue large pants', 10, 'M');
+
+    const bluePants = new Product();
     console.log(bluePants.toString());
-    
-    
+
 })()
