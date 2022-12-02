bfd3897350019ac633b485145e9f85f21f2a5905
DevHars <vargassanchezfabio01@gmail.com>
2022-12-02 12:47:32 -0500

    Herenci problematic 2.0

--- src/clean-code/06-classes-a.ts
+++ src/clean-code/06-classes-a.ts
@@ -4,16 +4,57 @@
     type Gender = 'M'|'F';
 
     class Person {
-
         constructor(
             public name: string, 
             public gender: Gender, 
             public birthdate: Date
         ) {}
     }
+    
+    class User extends Person {
+        
+        public lastAccess: Date;
 
+        constructor(
+            public email: string,
+            public role: string,
+            name: string,
+            gender: Gender,
+            birthdate: Date,
+        ) {
+            super( name, gender, birthdate)
+            this.lastAccess = new Date();
+        }
+        
+        checkCredentials() {
+            return true;
+        }
+    }
 
-    const newPerson = new Person('Fabio', 'M', new Date('2001-05-24'));
-    console.log( {newPerson} );
+    class UserSettings extends User {
+        constructor(
+            public workingDirectory : string,
+            public lastOpenFolder   : string,
+            email                   :string,
+            role                    : string,
+            name                    : string,
+            gender                  : Gender,
+            birthdate               : Date,
+        ){
+            super( email, role, name, gender, birthdate );
+        }
+    }
+
+    const userSettings = new UserSettings(
+        '/usr/home',
+        'home',
+        'fabio@gmail.com',
+        'Admin',
+        'Fabio',
+        'M',
+        new Date('2001-05-24')
+    );
+
+    console.log( { userSettings });
     
 })()
