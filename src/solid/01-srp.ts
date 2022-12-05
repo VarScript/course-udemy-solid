790545a7ae3c6db0d249bdfe1602d147788962fb
DevHars <vargassanchezfabio01@gmail.com>
2022-12-05 16:49:38 -0500

    SRP part 2

--- src/solid/01-srp.ts
+++ src/solid/01-srp.ts
@@ -15,36 +15,42 @@
         saveProduct( product: Product ) {
             console.log('Guardando en base de datos', product );
         }
-        
+
     }
 
     class Mailer {
+
         private masterEmail: string = 'fabio@gmail.com';
 
         sendEmail( emailList: string[], template: 'to-clients' | 'to-adminds') {
             console.log( 'Send emails to the clients', template );
-            
         }
+
     }
-    
-    // Usualmente, esto es una clase para controlar la vista que es desplegada al usuario
-    // Recuerden que podemos tener muchas vistas que realicen este mismo trabajo.
+
+
     class ProductBloc {
-    
+
+        private productService: ProductService;
+        private mailer: Mailer;
+
+        constructor( productService: ProductService, mailer: Mailer ) {
+            this.productService = productService;
+            this.mailer = mailer;
+        }
+
         loadProduct( id: number ) {
-            // Realiza un proceso para obtener el producto y retornarlo
-            console.log('Producto: ',{ id, name: 'OLED Tv' });
+            this.productService.getProduct( id );
         }
-    
+
         saveProduct( product: Product ) {
-            // Realiza una petición para salvar en base de datos 
-            console.log('Guardando en base de datos', product );
+            this.productService.saveProduct( product );
         }
-    
+
         notifyClients() {
-            console.log('Enviando correo a los clientes');
+            this.mailer.sendEmail(['eduardo@google.com'], 'to-clients')
         }
-    
+
     }
 
     class CartBloc{
@@ -57,22 +63,19 @@
         }
 
     }
-    
+
+    const productService = new ProductService();
+    const mailer = new Mailer();
 
 
-    const productBloc = new ProductBloc();
+    const productBloc = new ProductBloc( productService, mailer);
     const cardBloc    =new CartBloc();
+
 
     productBloc.loadProduct(10);
     productBloc.saveProduct({ id: 10, name: 'OLED TV' });
     productBloc.notifyClients();
     cardBloc.addToCart(10);
-
-
-
-
-
-
 
 
 })();
